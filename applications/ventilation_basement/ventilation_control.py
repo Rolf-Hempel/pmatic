044272cb037b5eb708544c20c3ebe1d676303e31
rhem <rolf.hempel@dlr.de>
2016-07-13 19:31:47 +0200

    Ventilation control application extended

--- applications/ventilation_basement/ventilation_control.py
+++ applications/ventilation_basement/ventilation_control.py
@@ -18,6 +18,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import datetime
 import sys
 import time
 
@@ -25,6 +26,15 @@ import pmatic
 
 
 class temperature_humidity(object):
+    """This class manages the readouts from internal and external temperature/humidity meters. For both internal and
+    external measurements, the readouts from several devices can be averaged.
+
+    The temperature_humidity object keeps a list of external temperature readings, each stored together with the
+    corresponding Unix timestamp. Readings which are more than one day old are dropped from the list. This list is the
+    basis for computing the maximum and minimum temperatures.
+
+    """
+
     def __init__(self, temperature_devices_external, temperature_devices_internal):
         self.temperature_devices_external = temperature_devices_external
         self.temperature_devices_internal = temperature_devices_internal
@@ -34,6 +44,12 @@ class temperature_humidity(object):
         self.update_temperature_humidity()
 
     def update_temperature_humidity(self):
+        """
+        Read the current values for external / internal temperature and humidity and update minimum and maximum
+        temperatures.
+        :return: current values for internal and external temperature and humidity, maximum external temperature
+        and the Unix timestamp of the minimum external temperature, both computed for the last 24 hours.
+        """
         current_temperature_external = 0.
         current_humidity_external = 0.
         for device in self.temperature_devices_external:
@@ -48,16 +64,20 @@ class temperature_humidity(object):
             current_humidity_internal += device.humidity.value
         current_temperature_internal = current_temperature_internal / len(self.temperature_devices_internal)
         current_humidity_internal = current_humidity_internal / len(self.temperature_devices_internal)
+        # Unix timestamp (counted in seconds since 1970.0)
         current_time = time.time()
         temp_object = [current_time, current_temperature_external]
         self.temperatures.append(temp_object)
 
-        for i in range(len(self.temperatures)):
-            if current_time - self.temperatures[i][0] < 86400.:
-                break
-        self.temperatures = self.temperatures[i:]
-
+        # Update minimum and maximum temperature only once per day
         if current_time - self.minmax_time_updated >= 86400.:
+            if len(self.temperatures) > 0:
+                # Drop entries older than one day
+                for i in range(len(self.temperatures)):
+                    if current_time - self.temperatures[i][0] < 86400.:
+                        break
+                self.temperatures = self.temperatures[i:]
+
             self.minmax_time_updated = current_time
             self.index_max = -1
             self.index_min = -1
@@ -70,11 +90,25 @@ class temperature_humidity(object):
                 if to[1] < self.min_temperature:
                     self.min_temperature = to[1]
                     self.min_temperature_time = to[0]
+            print "\n", date_and_time(), \
+                " Updating minimum and maximum external temperatures:\nCurrent temperature: ", \
+                current_temperature_external, ", New minimum temperature: ", \
+                self.min_temperature, ", new maximum temperature: ", self.max_temperature, "\n"
         return (current_temperature_internal, current_humidity_internal, current_temperature_external,
                 current_humidity_external, self.max_temperature, self.min_temperature_time)
 
 
 class switch(object):
+    """This class controls the switch(es) of the ventilation devic(es). Basically, a ventilator is switched on only if
+    the outside dew point temperature is below the inside air temperature. Additionally, the ventilator is switched on
+    for periods of 1000 seconds each, with pauses in between. The distribution of the active periods over the day is
+    defined by a switch pattern. Two different patterns are set: The first one is applied if the maximum outside
+    temperature is below a certain threshold. In order to get as warm air as possible into the room, more active periods
+    occur during the day than at night. If the outside temperature is high, ventilation takes place preferably during
+    the night.
+
+    """
+
     def __init__(self, switch_devices):
         self.switch_devices = switch_devices
         self.low_temp_pattern = [[10000., 11000.], [20000., 21000.], [29000., 30000.], [35000., 36000.], \
@@ -83,10 +117,22 @@ class switch(object):
         self.high_temp_pattern = [[3000., 4000.], [8000., 9000.], [13000., 14000.], [18000., 19000.], \
                                   [26000., 27000.], [36000., 37000.], [46000., 47000.], [56000., 57000.], \
                                   [61000., 62000.], [69000., 70000.], [77000., 78000.], [83000., 84000.]]
+        # Definition of the threshold temperature
         self.transition_temperature = 10.
 
     def ventilator_state_update(self, current_temperature_internal, current_temperature_external,
                                 current_humidity_external, max_temperature, min_temperature_time):
+        """
+        Switch the ventilator(s) on/off depending on current temperature and humidity measurements and information about
+        the maximum temperature and the time when the minimum temperature was attained.
+
+        :param current_temperature_internal: Current internal air temperature (Celsius)
+        :param current_temperature_external: Current outside air temperature (Celsius)
+        :param current_humidity_external: Current outside relative humidity (0. for 0%, 1. for 100%)
+        :param max_temperature: Maximal outside air temperature during the last 24 hours (Celsius)
+        :param min_temperature_time: Unix timestamp of minimal outside air temperature during the last 24 hours
+        :return: -
+        """
         if max_temperature > self.transition_temperature:
             temp_pattern = self.high_temp_pattern
         else:
@@ -96,30 +142,47 @@ class switch(object):
             if interval[0] < time.time() - min_temperature_time < interval[1]:
                 switch_on_time = True
                 break
+        dew_point_external = pmatic.utils.dew_point(current_temperature_external, current_humidity_external)
         for switch_device in self.switch_devices:
             if switch_device.is_on() and not switch_on_time:
+                print date_and_time(), " Switching ", switch_device.name, " off"
                 switch_device.switch_off()
-            elif switch_device.is_off() and pmatic.utils.dew_point(current_temperature_external,
-                                            current_humidity_external) < current_temperature_internal:
+            elif switch_on_time and switch_device.is_off() and dew_point_external < current_temperature_internal:
+                print date_and_time(), " Switching ", switch_device.name, " on, T int: ", \
+                    current_temperature_internal, ", T ext: ", current_temperature_external, \
+                    ", Dew point: ", dew_point_external
                 switch_device.switch_on()
+
+
+def date_and_time():
+    """Compute the current date and time.
+
+    :return: Character string with current date and time information
+    """
+    return datetime.datetime.fromtimestamp(time.time())
 
 
 if __name__ == "__main__":
     # ccu = pmatic.CCU()
-    # sys.stdout = open('/media/sd-mmcblk0/protocols/ventilation', 'a')
+    # sys.stdout = open('/media/sd-mmcblk0/protocols/ventilation.txt', 'a')
     ccu = pmatic.CCU(address="http://192.168.0.51", credentials=("rolf", "Px9820rH"), connect_timeout=5)
+
+    # Look up outside and internal temperature/humidity measuring devices
+    print "\n", date_and_time(), " Starting ventilation control program", , "\nDevices used:"
     temperature_devices_external = ccu.devices.query(device_type=[u'HM-WDS10-TH-O'])
     for device in temperature_devices_external:
         print "External temperature device: ", device.name
     if len(temperature_devices_external) == 0:
         print "Error: No external thermometer found."
         sys.exit(1)
+    # Add selection of room!
     temperature_devices_internal = ccu.devices.query(device_type=[u'HM-WDS40-TH-I-2'])
     for device in temperature_devices_internal:
         print "Internal temperature device: ", device.name
     if len(temperature_devices_internal) == 0:
         print "Error: No external thermometer found."
         sys.exit(1)
+    # Look up name of switch devices
     switch_devices = ccu.devices.query(device_name=u"Steckdosenschalter Gartenkeller")
     for device in switch_devices:
         print "Switch device: ", device.name
@@ -127,13 +190,13 @@ if __name__ == "__main__":
         print "Error: No switch device found."
         sys.exit(1)
 
-    temperature_external_object = temperature_humidity(temperature_devices_external, temperature_devices_internal)
-    switch_object = switch(switch_devices)
+    th = temperature_humidity(temperature_devices_external, temperature_devices_internal)
+    sw = switch(switch_devices)
 
     while True:
-        current_temperature_internal, current_humidity_internal, current_temperature_external,\
+        current_temperature_internal, current_humidity_internal, current_temperature_external, \
         current_humidity_external, max_temperature, min_temperature_time = \
-            temperature_external_object.update_temperature_humidity()
-        switch_object.ventilator_state_update(current_temperature_internal, current_temperature_external,
-                                              current_humidity_external, max_temperature, min_temperature_time)
+            th.update_temperature_humidity()
+        sw.ventilator_state_update(current_temperature_internal, current_temperature_external,
+                                   current_humidity_external, max_temperature, min_temperature_time)
         time.sleep(120)
