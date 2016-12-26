a1c8ce88c422f0262701f726ac096d27f0702624
Rolf-Hempel <rolf6419@gmx.de>
2016-12-26 09:07:13 +0100

    New modules for system_variables and home_control created. Documentation updated

---
+++ applications/home_control.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Application code for pmatic - Python API for Homematic. Easy to use.
+# Copyright (C) 2016 Rolf Hempel <rolf6419@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import codecs
+import os.path
+
+import pmatic.api
+from brightness import brightness
+from miscellaneous import *
+from parameters import parameters
+from shutter_control import windows
+from sun_position import sun_position
+from system_variables import sysvar_activities
+from temperature import temperature
+
+#
+# This is the main control program for all smart home applications. Depending on whether the program is run on the CCU2
+# or on a remote computer, it sets the file names for input and protocol files (for remote execution, protocol output
+# is directed to stdout.) accordingly.
+#
+# Next, all control objects are created and initialized.
+#
+# Finally, the (infinite) main loop is started. First, parameter changes are detected. This gives the user a way to
+# change parameters without interrupting the program. Then all objects are updated, and periodic activities (such as
+# adjusting the shutters) are started. At the end of the main loop, a wait period is inserted to limit the CPU load.
+#
+
+if __name__ == "__main__":
+
+
+    # Depending on whether the program is executed on the CCU2 itself or on a remote PC, the parameters are stored at
+    # different locations.
+    ccu_parameter_file_name = "/etc/config/addons/pmatic/scripts/applications/parameter_file"
+    remote_parameter_file_name = "/home/rolf/Pycharm-Projects/pmatic/applications/parameter_file"
+    ccu_temperature_file_name = "/etc/config/addons/pmatic/scripts/applications/temperature_file"
+    remote_temperature_file_name = "/home/rolf/Pycharm-Projects/pmatic/applications/temperature_file"
+
+    # Test if the remote parameter file is found. In this case the program runs on a remote computer.
+    if os.path.isfile(remote_parameter_file_name):
+        params = parameters(remote_parameter_file_name)
+        temperature_file_name = remote_temperature_file_name
+        if params.output_level > 0:
+            print ""
+            print_output(
+                "++++++++++++++++++++++++++++++++++ Start Remote Execution on PC +++++++++++++++++++++++++++++++++++++")
+        ccu = pmatic.CCU(address=params.ccu_address, credentials=(params.user, params.password), connect_timeout=5)
+        api = pmatic.api.init(address=params.ccu_address, credentials=(params.user, params.password))
+    else:
+        # Wait for CCU startup to be completed
+        # time.sleep(10.)
+        params = parameters(ccu_parameter_file_name)
+        temperature_file_name = ccu_temperature_file_name
+        # For execution on CCU redirect stdout to a protocol file
+        sys.stdout = codecs.open('/media/sd-mmcblk0/protocols/shutter_control.txt', encoding='utf-8', mode='a')
+        if params.output_level > 0:
+            print ""
+            print_output(
+                "++++++++++++++++++++++++++++++++++ Start Local Execution on CCU +++++++++++++++++++++++++++++++++++++")
+        ccu = pmatic.CCU()
+        api = pmatic.api.init()
+
+    if params.output_level > 0:
+        params.print_parameters()
+
+    # Create the object which stores parameters for computing the sun's location in the sky
+    sun = sun_position(params)
+
+    # Create the object which defines shutter setting activities controlled by system variables
+    sysvar_act = sysvar_activities(params, api)
+
+    # Create window dictionary and initialize parameters for all windows
+    windows = windows(params, ccu, sysvar_act, sun)
+
+    # Create the object which keeps the current temperature and maximum/minimum values during the previous day
+    temperatures = temperature(params, ccu, temperature_file_name)
+
+    # Create the object which looks up the current brightness level and holds the maximum value during the last hour
+    brightnesses = brightness(params, ccu)
+    # If "temperatures" and "brightnesses" are created, the temperature and brightness devices could be accessed.
+
+    # Main loop
+    while True:
+        # Read parameter file, check if since the last iteration parameters have changed.
+        # If parameters have changed, create a new sun object. Otherwise just update sun position.
+        if params.update_parameters():
+            if params.output_level > 0:
+                print_output("\nParameters have changed!")
+                params.print_parameters()
+            # Reset time stamp for last test for sunrise/sunset. This is necessary because conditions might have
+            # changed if, for example, the geographic position is changed.
+            sun.sun_is_up_last_changed = 0.
+        # Update sun position
+        sun.update_position()
+        # Update the temperature info
+        temperatures.update()
+        # Update the brightness info
+        brightnesses.update()
+        # Update the system variable setting
+        sysvar_act.update()
+        # Set all shutters corresponding to the actual temperature and brightness conditions.
+        windows.adjust_all_shutters(temperatures, brightnesses)
+        # Add a delay before the next main loop iteration
+        time.sleep(params.main_loop_sleep_time)
--- applications/shutter_control.py
+++ applications/shutter_control.py
@@ -20,137 +20,15 @@
 
 import codecs
 import os.path
-from math import radians, degrees
+from math import radians
 
 import pmatic.api
 from brightness import brightness
 from miscellaneous import *
 from parameters import parameters
 from sun_position import sun_position
+from system_variables import sysvar_activities
 from temperature import temperature
-
-
-class sysvar_activities(object):
-    """This class defines shutter setting activities controlled by system variables.
-
-    This makes it possible to trigger activities from outside this program, e.g. by setting a system variable with
-    a HomeMatic remote control unit.
-
-    """
-
-    def __init__(self, params, api):
-        """
-        Define the activities and their corresponding data structures.
-
-        :param params: parameter object
-        :param api: pmatic object which gives access to the JSON RPC Api of HomeMatic
-        :return: -
-        """
-        self.api = api
-        self.params = params
-        self.suspend_shutter_activities = {"name": u'Keine Rolladenbewegungen', "active": False}
-        self.suspend_sleeping_room = {"name": u'Keine RB Schlafzimmer', "active": False}
-        self.ventilate_upper = {"name": u'Lueften Obergeschoss', "active": False, "setting": 1.,
-                                "windows": [u'Schlafzimmer', u'Kinderzimmer', u'Badezimmer', u'Arbeitszimmer']}
-        self.ventilate_lower = {"name": u'Lueften Erdgeschoss', "active": False, "setting": 1.,
-                                "windows": [u'Wohnzimmer rechts', u'Küche rechts', u'Gäste-WC']}
-        self.ventilate_kitchen = {"name": u'Lueften Kueche', "active": False, "setting": 1.,
-                                  "windows": [u'Küche rechts', u'Gäste-WC']}
-        self.ventilate_night = {"name": u'Lueften Nacht', "active": False, "setting": 1.,
-                                "windows": [u'Schlafzimmer', u'Kinderzimmer', u'Badezimmer', u'Arbeitszimmer',
-                                            u'Wohnzimmer rechts']}
-        self.shutter_constant_25 = {"name": u'Rollaeden 25 Prozent', "active": False, "setting": 0.25,
-                                    "windows": [u'Schlafzimmer', u'Kinderzimmer', u'Badezimmer', u'Arbeitszimmer',
-                                                u'Wohnzimmer rechts', u'Wohnzimmer links', u'Küche rechts',
-                                                u'Küche links', u'Gäste-WC', u'Terrassentür', u'Terrassenfenster']}
-        self.shutter_constant_50 = {"name": u'Rollaeden 50 Prozent', "active": False, "setting": 0.5,
-                                    "windows": [u'Schlafzimmer', u'Kinderzimmer', u'Badezimmer', u'Arbeitszimmer',
-                                                u'Wohnzimmer rechts', u'Wohnzimmer links', u'Küche rechts',
-                                                u'Küche links', u'Gäste-WC', u'Terrassentür', u'Terrassenfenster']}
-        self.shutter_constant_100 = {"name": u'Rollaeden 100 Prozent', "active": False, "setting": 1.,
-                                    "windows": [u'Schlafzimmer', u'Kinderzimmer', u'Badezimmer', u'Arbeitszimmer',
-                                                u'Wohnzimmer rechts', u'Wohnzimmer links', u'Küche rechts',
-                                                u'Küche links', u'Gäste-WC', u'Terrassentür', u'Terrassenfenster']}
-        self.tv_evening = {"name": u'Fernsehabend', "active": False, "setting": 0.,
-                           "windows": [u'Wohnzimmer rechts', u'Wohnzimmer links', u'Terrassentür', u'Terrassenfenster']}
-        self.ventilate_until_morning = {"name": u'Lueften bis zum Morgen', "active": False}
-        self.sysvar_ventilation_activities = [self.ventilate_upper, self.ventilate_lower,
-                                              self.ventilate_kitchen, self.ventilate_night]
-        self.sysvar_shutter_activities = [self.tv_evening, self.ventilate_upper, self.ventilate_lower,
-                                          self.ventilate_kitchen, self.ventilate_night]
-        self.constant_daytime_shutter_settings = [self.shutter_constant_25, self.shutter_constant_50,
-                                                 self.shutter_constant_100]
-        self.sysvars = {u'Keine Rolladenbewegungen': self.suspend_shutter_activities, u'Fernsehabend': self.tv_evening,
-                        u'Lueften Obergeschoss': self.ventilate_upper, u'Lueften Erdgeschoss': self.ventilate_lower,
-                        u'Lueften Kueche': self.ventilate_kitchen, u'Lueften Nacht': self.ventilate_night,
-                        u'Keine RB Schlafzimmer': self.suspend_sleeping_room,
-                        u'Lueften bis zum Morgen': self.ventilate_until_morning,
-                        u'Rollaeden 25 Prozent': self.shutter_constant_25,
-                        u'Rollaeden 50 Prozent': self.shutter_constant_50,
-                        u'Rollaeden 100 Prozent': self.shutter_constant_100}
-
-    def update(self):
-        """
-        Update the system variable settings
-        :return: -
-        """
-        for sysvar_name, activity in self.sysvars.iteritems():
-            # The pmatic api returns the values "true" and "false" as character strings instead of a boolean!
-            new_value = self.api.sys_var_get_value_by_name(name=sysvar_name) == "true"
-            if activity["active"] != new_value:
-                if self.params.output_level > 1:
-                    print_output("System variable " + sysvar_name + " has changed to " + str(new_value))
-                activity["active"] = self.api.sys_var_get_value_by_name(name=sysvar_name) == "true"
-
-    def sysvar_induced_setting(self, window_name):
-        """
-        For a window with name "window_name": Find out if it is included in an activity selected by an active system
-        variable. For the first match, return the corresponding shutter setting.
-
-        :param window_name:
-        :return: sysvar-induced setting value, or None
-        """
-        for activity in self.sysvar_shutter_activities:
-            if activity["active"] and window_name in activity["windows"]:
-                return activity["setting"]
-        return None
-
-    def constant_daytime_setting(self, window_name):
-        """
-        For a window with name "window_name": Find out if a constant daytime setting is selected for it by an active
-        system variable. For the first match, return the corresponding shutter setting. If there is no match,
-        return None.
-
-        :param window_name:
-        :return: constant daytime setting value, or None
-        """
-        for activity in self.constant_daytime_shutter_settings:
-            if activity["active"] and window_name in activity["windows"]:
-                return activity["setting"]
-        return None
-
-    def shutter_activities_suspended(self):
-        """
-        If the boolean system variable "Keine Rolladenbewegungen" is set to "true", all shutter movements are suspended.
-        :return: True if movements are suspended, otherwise False
-        """
-        return self.suspend_shutter_activities["active"]
-
-    def reset_ventilation_in_the_morning(self):
-        """
-        If the boolean system variable "Lueften bis zum Morgen" is set to "true", all nocturnal ventilation actions
-        are continued until the shutters are opened in the morning. At this point all ventilation actions are reset,
-        and the system variable is reset to "False".
-        :return: -
-        """
-        if self.ventilate_until_morning["active"]:
-            if self.params.output_level > 1:
-                print_output("Reset ventilation activities in the morning")
-            for activity in self.sysvar_ventilation_activities:
-                self.api.sys_var_set_float(name=activity["name"], value=False)
-                activity["active"] = False
-            self.api.sys_var_set_float(name=self.ventilate_until_morning["name"], value=False)
-            self.ventilate_until_morning["active"] = False
 
 
 class window(object):
