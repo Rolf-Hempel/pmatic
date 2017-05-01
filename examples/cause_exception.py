b07d286ec743d2faf37fd6a1419e953a14fdd60f
Rolf-Hempel <rolf6419@gmx.de>
2017-05-01 08:33:19 +0200

    test program to cause an exception after 10 seconds added

---
+++ examples/cause_exception.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# pmatic - Python API for Homematic. Easy to use.
+# Copyright (C) 2016 Lars Michelsen <lm@larsmichelsen.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import pmatic
+import time
+
+# Open up a remote connection via HTTP to the CCU and login as admin. When the connection
+# can not be established within 5 seconds it raises an exception.
+ccu = pmatic.CCU(
+    # TODO: Replace this with the URL to your CCU2.
+    address="http://192.168.0.51",
+    # TODO: Insert your credentials here.
+    credentials=("rolf", "Px9820rH"),
+    connect_timeout=5
+)
+
+print "This program will cause an exception in 10 seconds."
+time.sleep(10.)
+a = 1. / 0.
